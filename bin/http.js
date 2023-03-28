f58a9e3a4e954561296f35cc2cd125ec9fd55318
jdrydn <james@jdrydn.com>
2023-03-28 21:57:21 +0100

    Updated

---
+++ bin/http.js
@@ -0,0 +1,37 @@
+#!/usr/bin/env node
+const debug = require('debug')('aws-lambda-rie-http');
+
+void (async function main() {
+  const { getConfig } = await import('../lib/config.mjs');
+  const { events, contexts } = await import('../lib/lambda.mjs');
+  const { getHandler } = await import('../lib/handlers.mjs');
+  const { createServer } = await import('../lib/server.mjs');
+
+  const config = getConfig({ argv: process.argv });
+  debug('config', config);
+
+  try {
+    const { handlerName, httpHost, httpPort, eventFormat, contextFormat } = config;
+
+    const { [eventFormat]: createEvent } = events;
+    const { [contextFormat]: createContext } = contexts;
+    const handlerFn = await getHandler(handlerName);
+
+    const server = createServer({ createEvent, createContext, handlerName, handlerFn });
+
+    ['SIGTERM', 'SIGUSR2'].forEach(event => process.once(event, async () => {
+      await server.stop().catch(err2 => console.error(err2));
+      process.kill(process.pid);
+    }));
+    ['uncaughtException', 'unhandledRejection'].forEach(event => process.once(event, async (err1) => {
+      console.error(err1);
+      await server.stop().catch(err2 => console.error(err2));
+      process.kill(process.pid);
+    }));
+
+    await server.start({ httpHost, httpPort });
+  } catch (err) {
+    console.error(err);
+    process.exit(1);
+  }
+})();
